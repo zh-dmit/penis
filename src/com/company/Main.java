e2a43ec1eb88249a0bbb3c236f216fef9b1a2cdd
Евген <zh-dmit@users.noreply.github.com>
2018-07-04 22:41:44 +0300

    bild_1.0

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,30 +5,22 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int pole[] = new int[9];
-        int i = 0;
-        while (i < 9){
-            System.out.println("Ход игрока 1, введите 1(x) или 2(o) ");
-            Scanner scan = new Scanner(System.in);
-            pole[i] = scan.nextInt();
-            i++;
-            for (int v:pole)
-            System.out.print(v+"|");
-            if(i==3) break;
-            System.out.println("Ход игрока 2, введите 1(x) или 2(o) ");
-            pole[i] = scan.nextInt();
-            for (int v:pole)
-            System.out.print(v+"|");
-            i++;
+        int pole[][] = new int[3][3];
+        System.out.println("Игрок 1 введите координаты поля: ");
+        System.out.println(" 0|0|1|2|");
+        System.out.println(" 0|_|_|_|");
+        System.out.println(" 1|_|_|_|");
+        System.out.println(" 2|_|_|_|");
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        pole [a][b] = scan.nextInt();
+        System.out.println(" 0|0|1|2|");
+        System.out.println(" 0|"+pole[0][0]+"|"+pole[0][1]+"|"+pole[0][2]+"|");
+        System.out.println(" 1|"+pole[1][0]+"|"+pole[1][1]+"|"+pole[1][2]+"|");
+        System.out.println(" 1|"+pole[2][0]+"|"+pole[2][1]+"|"+pole[2][2]+"|");
 
 
-        }
-        for (int v:pole)
-        System.out.print(v);
-
-
-
-
-	// write your code here
     }
+
 }
